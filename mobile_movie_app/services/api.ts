13c6b397c5064e605497ab459496812feeeeaf13
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-08-04 15:45:21 +0300

    services api added

---
+++ mobile_movie_app/services/api.ts
@@ -0,0 +1,15 @@
+// const url =
+//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzc3NDNmYmM1YmVhNmE0ZGFkYjM5NWNmMWZiYTk3MiIsIm5iZiI6MTc1NDMwODU5Ny45NTM5OTk4LCJzdWIiOiI2ODkwOWZmNTU3MTI1Y2UzOWY2MWY0MmMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7qWNKmSirFXhDIgVs7xSU_EJzA6AsjQUWHo02d63bgk",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
